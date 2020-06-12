b19bc5c0a48f3f80e47ef498d0361f3d450262f4
GopalDas123 <Gopaldax@gmail.com>
2020-06-13 01:28:14 +0500

    First Go

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './new.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(data){
+  return <div>
+    <center><h1 className="data">This Is Made By {data.name} & My Age Is {data.age -5} </h1></center>
+  </div>
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div><App name="Gopal Das" age={26} /> </div>,
   document.getElementById('root')
 );
 
